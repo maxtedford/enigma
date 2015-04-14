639370c55b1a135ea96807dd1d84c43dd754b408
Max Tedford <max.tedford@gmail.com>
2015-04-14 12:46:36 -0600

    added an assigner class and tweaked the slicer

--- test/slicer_test.rb
+++ test/slicer_test.rb
@@ -14,8 +14,18 @@ class SlicerTest < MiniTest::Test
     assert_equal "string", slice.string
   end
 
-  def test_that_it_can_slice_an_eight_character_string_into_two_rows
+  def test_that_it_can_slice_an_eight_character_string_into_two_split_arrays
     slice = Slicer.new("String!!")
     assert_equal [["S", "t", "r", "i"], ["n", "g", "!", "!"]], slice.slice
+  end
+
+  def test_that_it_can_slice_an_odd_number_array_and_leave_the_remainder
+    slice = Slicer.new("String")
+    assert_equal [["S", "t", "r", "i"], ["n", "g"]], slice.slice
+  end
+
+  def test_that_it_can_slice_a_super_long_uneven_character_string_into_many_smaller_split_arrays
+    slice = Slicer.new("I am a really long string")
+    assert_equal [["I", " ", "a", "m"], [" ", "a", " ", "r"], ["e", "a", "l", "l"], ["y", " ", "l", "o"], ["n", "g", " ", "s"], ["t", "r", "i", "n"], ["g"]], slice.slice
   end
 end
